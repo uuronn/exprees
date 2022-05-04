8026428cbd80d0d571dfb1244c1da50cb28c18eb
uuronn <uuronprogram@gmail.com>
2022-05-05 02:21:28 +0900

    Create: users

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 // テンプレートエンジンの指定
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  // console.log("test");
+  // console.log(req.query.name);
+  // res.send("root");
+  res.json();
+});
+
 app.get("/todo", function (req, res) {
-  var data = {
+  const data = {
     items: [
       { name: "<h1>リンゴ</h1>" },
-      { name: "<h2>バナナ</h2>" },
+      { name: "パイン" },
       { name: "<h4>スイカ</h4>" },
     ],
   };
@@ -17,4 +24,16 @@ app.get("/todo", function (req, res) {
   // res.send(data);
 });
 
-app.listen(3010);
+app.get("/api/users", function (req, res, next) {
+  const db = {
+    users: [
+      { name: "taro", age: 16, height: 160 },
+      { name: "syu", age: 17, height: 170 },
+      { name: "yui", age: 18, height: 180 },
+    ],
+  };
+  res.render("./index.ejs", db);
+  // res.json(db);
+});
+
+console.log("http://localhost:" + app.listen(3010).address().port);
