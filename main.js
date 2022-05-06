e8bc0a12d4b67ce37d3e661c57e2ed397a26e286
uuronn <uuronprogram@gmail.com>
2022-05-06 18:17:54 +0900

    Create: db

--- main.js
+++ main.js
@@ -2,36 +2,30 @@ const express = require("express");
 const app = express();
 
 // テンプレートエンジンの指定
-
 app.set("view engine", "ejs");
+
+// consoleに表示するための設定
 app.use(express.urlencoded());
 app.use(express.json());
 
 const db = {
-  users: {
-    name: "issei",
-  },
-  todos: [{ title: "タスク1" }],
+  todos: [{ title: "タスク1" }, { title: "タスク2" }],
 };
 
-// app.get("/", function (req, res) {
-//   // レンダリングを行う
-//   res.render("./pages/index.ejs", db);
-//   // res.send(data);
-// });
+const sqlite3 = require("sqlite3");
+
+const dataBase = new sqlite3.Database("./express.db", (err) => {
+  console.error("database error: " + err.message);
+});
 
 app.get("/todo", function (req, res, next) {
+  // 1
   res.render("./pages/todo.ejs", { todos: db.todos });
 });
 
-app.post("/test", function (req, res, next) {
-  console.log(req.body);
-  res.render("./pages/todo.ejs", { todos: db.todos });
+app.post("/todo", function (req, res, next) {
+  db.todos.push({ title: req.body.title });
+  res.redirect("/todo");
 });
 
-app.get("/users", function (req, res, next) {
-  res.render("./pages/users.ejs", { users: db.users });
-  // res.json(db);
-});
-
-console.log("http://localhost:" + app.listen(3010).address().port);
+console.log("http://localhost:" + app.listen(3010).address().port + "/todo");
